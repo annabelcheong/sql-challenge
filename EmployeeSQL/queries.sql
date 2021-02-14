0d357e1ed69e66ef0f065a3de0f3a1852bcbebb6
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-02-14 12:31:57 +0800

    question1 completed and saved in queries.sql

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,3 +1,32 @@
+--QUESTION 1--
+-- List the following details of each employee: 
+-- employee number, last name, first name, sex, and salary.
+
+CREATE VIEW Q1 AS --Saved as a view 'Q1' in Database
+
+	SELECT emp.emp_no, emp.last_name, emp.first_name, emp.sex, sal.salary
+
+	FROM Employees emp
+
+	LEFT JOIN Salaries sal
+		ON emp.emp_no = sal.emp_no;   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 --IMPORT CSV FILES AND CREATE TABLES (6 TOTAL)
 --1. Departments
 CREATE TABLE departments(
