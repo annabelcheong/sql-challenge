7414447d1ce34d04a8423d5a1c104175fdb56e6b
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-02-14 11:31:20 +0800

    Completed ERD, exported postgres schemata query, imported csv files to postgres database

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -62,3 +62,12 @@ CREATE TABLE titles(
 --Check data output with select from
 SELECT *
 FROM titles;
+
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, sal.salary
+FROM employees as e
+WHERE emp_no IN(
+  	SELECT salary
+  	FROM salaries as sal
+	);
+  
---
+++ EmployeeSQL/schemata.sql
@@ -0,0 +1,68 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/WQHjF9
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+CREATE TABLE Departments (
+    dept_no varchar   NOT NULL,
+    dept_name varchar   NOT NULL,
+    CONSTRAINT pk_Departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE Emps_by_Dept (
+    emp_no int   NOT NULL,
+    dept_no varchar   NOT NULL
+);
+
+CREATE TABLE Department_Managers (
+    dept_no varchar   NOT NULL,
+    emp_no int   NOT NULL,
+    CONSTRAINT pk_Department_Managers PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Employees (
+    emp_no int   NOT NULL,
+    title_id varchar   NOT NULL,
+    birth_date date   NOT NULL,
+    first_name varchar   NOT NULL,
+    last_name varchar   NOT NULL,
+    sex varchar   NOT NULL,
+    hire_date date   NOT NULL,
+    CONSTRAINT pk_Employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Salaries (
+    emp_no int   NOT NULL,
+    salary int   NOT NULL
+);
+
+CREATE TABLE Titles (
+    title_id varchar   NOT NULL,
+    title varchar   NOT NULL,
+    CONSTRAINT pk_Titles PRIMARY KEY (
+        title_id
+     )
+);
+
+ALTER TABLE Emps_by_Dept ADD CONSTRAINT fk_Emps_by_Dept_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+ALTER TABLE Emps_by_Dept ADD CONSTRAINT fk_Emps_by_Dept_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
+
+ALTER TABLE Department_Managers ADD CONSTRAINT fk_Department_Managers_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
+
+ALTER TABLE Employees ADD CONSTRAINT fk_Employees_title_id FOREIGN KEY(title_id)
+REFERENCES Titles (title_id);
+
+ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+-- Import all csv files. 
+--Start from importing csv file with PK and no FK link constraint to another table that has not been loaded yet.
